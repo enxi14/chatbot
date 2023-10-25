75755e07d229e82403732a6af62aa4c9e51bd68f
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-25 19:34:20 -0400

    commit

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // JavaScript code goes here
 function sendMessage() {
-  const userInput = document.getElementById('userInput');
-  const chatbox = document.getElementById('chatbox');
+const userInput = document.getElementById('userInput');
+const chatbox = document.getElementById('chatbox');
 
-  const userMessage = userInput.value;
-  userInput.value = '';
+const userMessage = userInput.value;
+userInput.value = '';
 
-  chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div>';
+chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div>';
 
-  const botResponse = 'Bot: Thanks for your message!';
+const botResponse = 'Bot: Thanks for your message!';
 
-  chatbox.innerHTML += '<div class="chat-message bot-message">' + botResponse + '</div>';
+chatbox.innerHTML += '<div class="chat-message bot-message">' + botResponse + '</div>';
 
-  chatbox.scrollTop = chatbox.scrollHeight;
+chatbox.scrollTop = chatbox.scrollHeight;
 }
