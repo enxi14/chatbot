53369cfffaaa499dc904358518a2ea1e097e3e85
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-25 19:39:54 -0400

    commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// JavaScript code goes here
 function sendMessage() {
 const userInput = document.getElementById('userInput');
 const chatbox = document.getElementById('chatbox');
@@ -6,11 +5,25 @@ const chatbox = document.getElementById('chatbox');
 const userMessage = userInput.value;
 userInput.value = '';
 
-chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div>';
+chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div';
 
-const botResponse = 'Bot: Thanks for your message!';
+  // Respond to specific user input
+const botResponse = getBotResponse(userMessage);
 
-chatbox.innerHTML += '<div class="chat-message bot-message">' + botResponse + '</div>';
-
+chatbox.innerHTML += '<div class="chat-message bot-message">Bot: ' + botResponse + '</div>';
 chatbox.scrollTop = chatbox.scrollHeight;
+}
+
+function getBotResponse(userInput) {
+userInput = userInput.toLowerCase();
+
+if (userInput.includes('hello') || userInput.includes('hi')) {
+    return 'Hello there!';
+} else if (userInput.includes('how are you')) {
+    return 'I am just a bot, but I am functioning well. How can I assist you?';
+} else if (userInput.includes('bye')) {
+    return 'Goodbye! Have a great day!';
+} else {
+    return 'I didn\'t quite understand that. Could you please rephrase your message?';
+}
 }
