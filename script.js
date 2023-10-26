b7dddbbce3d12107090929e86e497a0c90f427d5
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-25 22:17:17 -0400

    commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const responses = {
+"hello": "Hello! How can I assist you today?",
+"hi": "Hi there! What can I help you with?",
+"how are you": "I'm just a bot, but I'm here to assist you. What do you need?",
+"what's your name": "I'm just a chatbot, so I don't have a name. You can call me Chatbot!",
+"bye": "Goodbye! If you have more questions in the future, feel free to ask.",
+};
+
 function sendMessage() {
 const userInput = document.getElementById('userInput');
 const chatbox = document.getElementById('chatbox');
@@ -5,25 +13,21 @@ const chatbox = document.getElementById('chatbox');
 const userMessage = userInput.value;
 userInput.value = '';
 
-chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div';
+chatbox.innerHTML += '<div class="chat-message user-message">You: ' + userMessage + '</div>';
 
-  // Respond to specific user input
-const botResponse = getBotResponse(userMessage);
+  // Normalize user input and find a response
+const botResponse = getBotResponse(userMessage.toLowerCase());
 
 chatbox.innerHTML += '<div class="chat-message bot-message">Bot: ' + botResponse + '</div>';
 chatbox.scrollTop = chatbox.scrollHeight;
 }
 
 function getBotResponse(userInput) {
-userInput = userInput.toLowerCase();
-
-if (userInput.includes('hello') || userInput.includes('hi')) {
-    return 'Hello there!';
-} else if (userInput.includes('how are you')) {
-    return 'I am just a bot, but I am functioning well. How can I assist you?';
-} else if (userInput.includes('bye')) {
-    return 'Goodbye! Have a great day!';
-} else {
-    return 'I didn\'t quite understand that. Could you please rephrase your message?';
+for (const key in responses) {
+    if (userInput.includes(key)) {
+    return responses[key];
+    }
 }
+
+return "I'm not sure how to respond to that. Could you please rephrase your message?";
 }
